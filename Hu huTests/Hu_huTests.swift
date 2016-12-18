39da4f6f24ef4fb573c7d97e05f6f42a560ffdb1
nguyenhungnq <NguyenHung@antonio.local>
2016-12-18 10:20:09 +0700

    Initial Commit

---
+++ Hu huTests/Hu_huTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hu_huTests.swift
+//  Hu huTests
+//
+//  Created by NguyenHung on 12/18/16.
+//  Copyright © 2016 NguyenHung. All rights reserved.
+//
+
+import XCTest
+@testable import Hu_hu
+
+class Hu_huTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
